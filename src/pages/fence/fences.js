52acb47ba2b253ab1144d8112c4f4d28b529f085
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-10-17 17:05:48 -0300

    separa estilo de conteúdo

--- src/pages/fence/fences.js
+++ src/pages/fence/fences.js
@@ -1,15 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import {
-  Alert,
-  FlatList,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import {Alert, FlatList, Text, TextInput, TouchableOpacity, View,} from 'react-native';
 import FenceService from '../../services/FenceService';
 import AuthenticationService from '../../services/config/AuthenticationService';
+import {styles} from './styles';
 
 const Fences = () => {
   const fenceService = new FenceService();
@@ -198,73 +191,6 @@ const Fences = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#ffffff',
-    alignItems: 'stretch',
-    justifyContent: 'flex-start',
-  },
-  register: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    backgroundColor: '#00ff00',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderWidth: 2,
-  },
-  item: {
-    flex: 3,
-    borderBottomWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text_header: {
-    color: '#000000',
-    fontSize: 35,
-    fontStyle: 'italic',
-    margin: 10,
-  },
-  text: {
-    color: '#000000',
-    fontSize: 20,
-    margin: 10,
-  },
-  text_item: {
-    color: '#000000',
-    fontSize: 35,
-    margin: 5,
-  },
-  input: {
-    width: '75%',
-    borderWidth: 1,
-    borderColor: '#555',
-    borderRadius: 5,
-    textAlign: 'center',
-    fontSize: 20,
-  },
-  button: {
-    width: 150,
-    height: 50,
-    backgroundColor: '#00ff00',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 5,
-  },
-  deleteButton: {
-    // flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#037272',
-    padding: 10,
-    borderRadius: 8,
-  },
-});
 
 
 export default Fences;
