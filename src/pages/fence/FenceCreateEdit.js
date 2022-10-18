ddc517661150a956a72983c223cd8b237e5a7648
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-10-18 07:27:12 -0300

    Move content from formRegisterFence() to return of the component

--- src/pages/fence/FenceCreateEdit.js
+++ src/pages/fence/FenceCreateEdit.js
@@ -47,67 +47,61 @@ const FenceCreateEdit = () => {
     });
   }
 
-  const formRegisterFence = () => {
-    return (
-      <View style={fenceStyles.register}>
-        <TextInput
-          style={fenceStyles.input}
-          placeholder="Nome da cerca"
-          onChangeText={value => setText(value)}
-          value={text}
-        />
-
-        <TextInput
-            style={fenceStyles.input}
-            placeholder="Latitude"
-            onChangeText={value => setLatitude(value)}
-            value={latitude}
-            keyboardType='numeric'
-        />
-
-        <TextInput
-            style={fenceStyles.input}
-            placeholder="Longitude"
-            onChangeText={value => setLongitude(value)}
-            value={longitude}
-            keyboardType='numeric'
-        />
-
-        <TextInput
-            style={fenceStyles.input}
-            placeholder="Raio"
-            onChangeText={value => setRadius(value)}
-            value={radius}
-            keyboardType='numeric'
-        />
-
-        <TextInput
-            style={fenceStyles.input}
-            placeholder="12:00"
-            onChangeText={value => setStartTime(value)}
-            value={startTime}
-        />
-
-        <TextInput
-            style={fenceStyles.input}
-            placeholder="18:00"
-            onChangeText={value => setEndTime(value)}
-            value={endTime}
-        />
-        <TouchableOpacity style={fenceStyles.button} onPress={onPressHandler}>
-          <Text style={fenceStyles.text}>Registrar</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  };
-
   return (
       <View style={fenceStyles.container}>
           <Text style={fenceStyles.title}>
               {fence ? "Edição de cerca" : "Criação de cerca"}
           </Text>
           <View style={fenceStyles.body}>
-              {formRegisterFence()}
+              <View style={fenceStyles.register}>
+                  <TextInput
+                      style={fenceStyles.input}
+                      placeholder="Nome da cerca"
+                      onChangeText={value => setName(value)}
+                      value={name}
+                  />
+
+                  <TextInput
+                      style={fenceStyles.input}
+                      placeholder="Latitude"
+                      onChangeText={value => setLatitude(value)}
+                      value={latitude}
+                      keyboardType='numeric'
+                  />
+
+                  <TextInput
+                      style={fenceStyles.input}
+                      placeholder="Longitude"
+                      onChangeText={value => setLongitude(value)}
+                      value={longitude}
+                      keyboardType='numeric'
+                  />
+
+                  <TextInput
+                      style={fenceStyles.input}
+                      placeholder="Raio"
+                      onChangeText={value => setRadius(value)}
+                      value={radius}
+                      keyboardType='numeric'
+                  />
+
+                  <TextInput
+                      style={fenceStyles.input}
+                      placeholder="12:00"
+                      onChangeText={value => setStartTime(value)}
+                      value={startTime}
+                  />
+
+                  <TextInput
+                      style={fenceStyles.input}
+                      placeholder="18:00"
+                      onChangeText={value => setEndTime(value)}
+                      value={endTime}
+                  />
+                  <TouchableOpacity style={fenceStyles.button} onPress={onPressHandler}>
+                      <Text style={fenceStyles.text}>{fence ? "SALVAR" : "REGISTRAR"}</Text>
+                  </TouchableOpacity>
+              </View>
           </View>
       </View>
 
