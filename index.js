fffa7351d4ecdf7f1cf0c81fb3a3ca9dacfdb216
Anderson S. Vieira <svieira.anderson@gmail.com>
2022-10-25 17:28:36 -0300

    cria função para checagem em segundo plano

--- index.js
+++ index.js
@@ -3,7 +3,25 @@
  */
 
 import {AppRegistry} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+});
+
+function HeadlessCheck({ isHeadless }) {
+    if (isHeadless) {
+        // App has been launched in the background by iOS, ignore
+        return null;
+    }
+
+    return App();
+}
+// linha nova
+AppRegistry.registerComponent(appName, () => HeadlessCheck);
+
+// linha original
+// AppRegistry.registerComponent(appName, () => App);
